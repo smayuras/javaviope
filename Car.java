3733d0ca93d54fc34a1f7c45abd87b99cca263c2
smayuras <smayuras@gmail.com>
2016-02-21 21:50:44 +0100

    Car class commit

---
+++ Car.java
@@ -0,0 +1,81 @@
+class Car{
+	
+	private String brand;
+	private String model;
+	private String registration_no;
+	private int yearmade;
+
+
+
+	public Car(){
+
+
+	}
+
+	public Car(String brand, String model, String registration_no, int yearmade){
+
+	 this.brand = brand;
+	 this.model = model;
+	 this.registration_no = registration_no;
+	 this.yearmade = yearmade;
+	}
+
+	public String getBrand(){
+
+	return this.brand;
+
+	}
+
+	public String getModel(){
+
+	return this.model;
+	}
+
+	public String getRegistrationNo(){
+
+	return this.registration_no;
+	}
+
+
+	public int getYearmade(){
+
+	return this.yearmade;
+	}
+
+
+	public void setBrand(String brand){
+
+	this.brand = brand;
+	}
+
+	public void setModel(String model){
+
+	this.model = model;
+	}
+
+	public void setRegistrationno(String registratio_no){
+
+	this.registration_no = registration_no;
+	}
+
+	public void setYearmade(int yearmade){
+
+	this.yearmade = yearmade;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
